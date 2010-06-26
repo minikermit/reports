d8670c6b383abef3363a8d965b1db7732d253698
minikermit <eyesonly202@gmail.com>
2010-06-26 18:04:26 +0200

    livefilter addition

--- app/models/account_plan.rb
+++ app/models/account_plan.rb
@@ -11,16 +11,26 @@ end
 
 
 
+
 # == Schema Information
 #
 # Table name: account_plans
 #
-#  id         :integer(4)      not null, primary key
-#  account_id :string(255)
-#  name       :string(255)
-#  comments   :string(255)     default("-")
-#  position   :integer(4)      default(1)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                :integer(4)      not null, primary key
+#  account_type_id   :integer(4)
+#  account_id        :string(255)
+#  shortname         :string(255)
+#  name              :string(255)
+#  position          :integer(4)      default(1)
+#  account_parent_id :string(255)
+#  parent_id         :integer(4)
+#  created_by        :string(255)
+#  updated_by        :string(255)
+#  group_id          :string(255)
+#  entity_id         :string(255)
+#  uo                :string(255)
+#  comments          :string(255)     default("-")
+#  created_at        :datetime
+#  updated_at        :datetime
 #
 
