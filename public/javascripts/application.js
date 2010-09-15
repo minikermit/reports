f3f1beb06eca2a8126334ec72b81746220603b3a
minikermit <eyesonly202@gmail.com>
2010-09-16 00:36:35 +0200

    Adding new form tests

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -79,3 +79,25 @@ $(document).ready(function() {
 	});		
 		
 });   
+
+// another styling trick for CSS select
+
+$(document).ready(function(){
+
+    if (!$.browser.opera) {
+
+        $('select.select').each(function(){
+            var title = $(this).attr('title');
+            if( $('option:selected', this).val() != ''  ) title = $('option:selected',this).text();
+            $(this)
+                .css({'z-index':10,'opacity':0,'-khtml-appearance':'none'})
+                .after('<span class="select">' + title + '</span>')
+                .change(function(){
+                    val = $('option:selected',this).text();
+                    $(this).next().text(val);
+                    })
+        });
+
+    };
+
+});
